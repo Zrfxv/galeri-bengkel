c379fe02eb50bd3acf400dc9e4611a76823fb475
Zrfxv <muhamadrizalsaputra99@gmail.com>
2023-07-21 23:41:06 +0700

    make customer model,migration,factory,etc

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorecustomerRequest;
+use App\Http\Requests\UpdatecustomerRequest;
+use App\Models\customer;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorecustomerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatecustomerRequest $request, customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(customer $customer)
+    {
+        //
+    }
+}
