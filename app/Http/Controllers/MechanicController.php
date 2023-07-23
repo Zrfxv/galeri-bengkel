ae079ee8601f8635337e3e5084c2af532a709c79
Zrfxv <muhamadrizalsaputra99@gmail.com>
2023-07-23 14:51:12 +0700

    Make Mechanic model

---
+++ app/Http/Controllers/MechanicController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreMechanicRequest;
+use App\Http\Requests\UpdateMechanicRequest;
+use App\Models\Mechanic;
+
+class MechanicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMechanicRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Mechanic $mechanic)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Mechanic $mechanic)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMechanicRequest $request, Mechanic $mechanic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Mechanic $mechanic)
+    {
+        //
+    }
+}
