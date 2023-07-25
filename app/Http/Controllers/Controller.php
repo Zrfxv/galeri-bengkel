541fae7878258cbf0e4edcee8b8698e9034e7052
Zrfxv <muhamadrizalsaputra99@gmail.com>
2023-07-25 16:13:40 +0700

    Testing database using view

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -9,4 +10,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function index()
+    {
+        return view('test', [
+            "title" => "Title Test",
+            "users" => User::all()
+        ]);
+    }
 }
