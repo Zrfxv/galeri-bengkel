d90ff4ec93d85b6c12c935f99ceab7477b98f1ac
irwanto1998 <rioirwanto9@gmail.com>
2023-07-28 00:59:55 +0700

    create a my profile view

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -37,7 +37,7 @@
                     <div class="dropdown-divider"></div>
                 </li>
                 <li>
-                    <a class="dropdown-item" href="#">
+                    <a href="" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#exLargeModal">
                         <i class="bx bx-user me-2"></i>
                         <span class="align-middle">My Profile</span>
                     </a>
@@ -50,10 +50,80 @@
                     <form action="{{ route('auth.logout') }}" method="post">
                     @csrf
                         {{-- <span class="align-middle">Log Out</span> --}}
-                        <button class="dropdown-item"><i class="bx bx-power-off me-2"></i> logout</button>
+                        <button class="dropdown-item"><i class="bx bx-power-off me-2"></i> Logout</button>
                     </form>
                 </li>
             </ul>
         </div>
     </nav>
+
+<!-- Extra Large Modal -->
+<div class="modal fade" id="exLargeModal" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-xl" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel4">My Profile</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        
+        <form action="#" method="POST">
+            @csrf
+            @method('PUT')
+        <div class="modal-body">
+              <div class="row mb-3">
+                <label class="col-sm-2 col-form-label" for="basic-default-name">Nama</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" name="name" id="basic-default-name" value="#"/>
+                </div>
+              </div>
+              <div class="row mb-3">
+                <label class="col-sm-2 col-form-label" for="basic-default-company">Username</label>
+                <div class="col-sm-10">
+                  <input
+                    type="text"
+                    class="form-control" name="username"
+                    id="basic-default-company"
+                    value="#"
+                  />
+                </div>
+              </div>
+              <div class="row mb-3">
+                <label class="col-sm-2 col-form-label" for="basic-default-company">Password</label>
+                <div class="col-sm-10">
+                  <input
+                    type="password"
+                    class="form-control" name="password"
+                    id="basic-default-company"
+                    placeholder=""
+                  />
+                </div>
+              </div>
+              <div class="row mb-3">
+                <label class="col-sm-2 col-form-label" for="basic-default-company">Role</label>
+                <div class="col-sm-10">
+
+                  <select class="form-select" name="role" id="exampleFormControlSelect1" aria-label="Default select example">
+                    <option>Silahkan Pilih </option>
+                    <option value="1" #>SPV</option>
+                    <option value="2" #>Admin</option>
+                  </select>
+                </div>
+              </div>
+          
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+            Close
+          </button>
+          <button type="button" class="btn btn-primary">Save</button>
+        </div>
+        </form>
+      </div>
+    </div>
+  </div>
  <!-- / Navbar -->
