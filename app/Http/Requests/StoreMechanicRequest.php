d55d4dfe80569fc0169f828999de5293774dc038
RoniBiaSanto <ronikun875@gmail.com>
2023-07-27 18:14:25 +0700

    add validation for store mechanic

--- app/Http/Requests/StoreMechanicRequest.php
+++ app/Http/Requests/StoreMechanicRequest.php
@@ -11,7 +11,7 @@ class StoreMechanicRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,12 @@ class StoreMechanicRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required',
+            'phone' => 'required',
+            'address' => 'required',
+            'level' => 'required',
+            'joined' => 'required',
+            'resign' => 'required',
         ];
     }
 }
