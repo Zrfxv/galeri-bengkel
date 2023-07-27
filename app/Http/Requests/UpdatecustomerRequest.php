ab2575f7a018093f6745841adb714fd418a76d2b
RoniBiaSanto <ronikun875@gmail.com>
2023-07-27 18:16:09 +0700

    add validation to update customer

--- app/Http/Requests/UpdatecustomerRequest.php
+++ app/Http/Requests/UpdatecustomerRequest.php
@@ -11,7 +11,7 @@ class UpdatecustomerRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,12 @@ class UpdatecustomerRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' =>  'required',
+            'phone' => 'required',
+            'email' =>  'required',
+            'birth' =>  'required',
+            'gender' =>  'required',
+            'address' =>  'required',
         ];
     }
 }
