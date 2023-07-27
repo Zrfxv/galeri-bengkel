5619f63279b80254494eb7ca13b4ab6287d076dc
RoniBiaSanto <ronikun875@gmail.com>
2023-07-27 11:45:20 +0700

    update auth controller in redirect

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -24,7 +24,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-            return redirect()->intended(RouteServiceProvider::class);
+            return redirect()->intended('/users');
         }
     }
     
