9e4061e36ef7451e8e97ec2b4f219479c5dd8fbd
RoniBiaSanto <ronikun875@gmail.com>
2023-07-27 11:43:34 +0700

    fix assets

--- resources/views/components/scp.blade.php
+++ resources/views/components/scp.blade.php
@@ -2,23 +2,23 @@
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
     
-    <script src="../assets/vendor/libs/jquery/jquery.js"></script>
-    <script src="../assets/vendor/libs/popper/popper.js"></script>
-    <script src="../assets/vendor/js/bootstrap.js"></script>
-    <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="{{ asset('assets') }}/vendor/libs/jquery/jquery.js"></script>
+    <script src="{{ asset('assets') }}/vendor/libs/popper/popper.js"></script>
+    <script src="{{ asset('assets') }}/vendor/js/bootstrap.js"></script>
+    <script src="{{ asset('assets') }}/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
 
-    <script src="../assets/vendor/js/menu.js"></script>
+    <script src="{{ asset('assets') }}/vendor/js/menu.js"></script>
     <!-- endbuild -->
 
     <!-- Vendors JS -->
 
     <!-- Main JS -->
-    <script src="../assets/js/main.js"></script>
+    <script src="{{ asset('assets') }}/js/main.js"></script>
 
     <!-- Page JS -->
 
     <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
     <!-- Datatable -->
-    <script src="../assets/vendor/datatables/js/jquery.dataTables.min.js"></script>
-    <script src="../assets/js/plugins-init/datatables.init.js"></script>
+    <script src="{{ asset('assets') }}/vendor/datatables/js/jquery.dataTables.min.js"></script>
+    <script src="{{ asset('assets') }}/js/plugins-init/datatables.init.js"></script>
