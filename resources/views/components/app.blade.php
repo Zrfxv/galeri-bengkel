7c2ac12c18ea2af725b16e0a3bdeb6760d12f756
irwanto1998 <rioirwanto9@gmail.com>
2023-07-28 00:49:05 +0700

    Change the website logo

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets') }}/img/favicon/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets') }}/img/logo.png" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
