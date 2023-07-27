e71f9c9dee40a6865ceca3f0cd5e23a72ed8108b
RoniBiaSanto <ronikun875@gmail.com>
2023-07-27 11:40:29 +0700

    add routes to create,update & delete users

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\UserController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -17,19 +18,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->to('/spv');
+    return redirect()->to(route('users.index'));
 })->middleware('auth');
 
 
 // ROUTE SPV
-Route::get('/spv', function () {
-    return view('spv.user.view' ,[
-        'users' => User::all()
-    ]);
-});
-Route::get('/tambah', function () {
-    return view('spv.user.tambah');
-});
+// Route::get('/spv', function () {
+//     return view('spv.user.view' ,[
+//         'users' => User::all()
+//     ]);
+// });
+// Route::get('/tambah', function () {
+//     return view('spv.user.tambah');
+// });
 
 // ROUTE Customer
 Route::get('/spv/customer', function () {
@@ -51,10 +52,17 @@ Route::get('/tambahmechanic', function () {
     return view('spv.mechanic.tambahMechanic');
 });
 
-
-Route::get('user', [Controller::class, 'index']);
-
-
+// ROUTE GROUP USERS ( SPV / ADMIN )
+Route::prefix('users')->group(function () {
+    
+    Route::get('/', [UserController::class, 'index'])->name('users.index');
+    Route::get('create', [UserController::class, 'create'])->name('users.create');
+    Route::post('create', [UserController::class, 'store'])->name('users.store');
+    
+    Route::get('edit/{user}', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('edit/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('delete/{user}', [UserController::class, 'destroy'])->name('users.delete');
+});
 
 // ROUTE LOGIN & LOGOUT
 Route::get('/login', [AuthController::class, 'index'])->name('auth.index');
