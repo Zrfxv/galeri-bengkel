b69fec96e6c92570e3c58d74583b231c8302161e
irwanto1998 <rioirwanto9@gmail.com>
2023-07-29 02:20:49 +0700

    add a confirmation delete data

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -169,3 +169,22 @@
       </div>
   </div>
 </div>
+
+<!-- Delete Confirmation Modal -->
+<div class="modal fade" id="deleteConfirmationModal" tabindex="-1" role="dialog" aria-labelledby="deleteConfirmationModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+      <div class="modal-content">
+          <div class="modal-header">
+              <h5 class="modal-title" id="deleteConfirmationModalLabel">Konfirmasi</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+              Yakin ingin menghapus data ini ?
+          </div>
+          <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+              <button type="button" class="btn btn-danger" id="deleteButton">Delete</button>
+          </div>
+      </div>
+  </div>
+</div>
