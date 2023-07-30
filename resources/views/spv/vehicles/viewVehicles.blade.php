65d849291e3cb49b5836f519bc03a73a68b78694
irwanto1998 <rioirwanto9@gmail.com>
2023-07-30 13:13:26 +0700

    add a notification vehicles

--- resources/views/spv/vehicles/viewVehicles.blade.php
+++ resources/views/spv/vehicles/viewVehicles.blade.php
@@ -25,6 +25,29 @@
               <div class="container-xxl flex-grow-1 container-p-y">
                 <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Data /</span> Vehicles</h4>
 
+                @if(session('Vehi-success'))
+                <script>
+                    // Fungsi untuk menampilkan popup notifikasi
+                    function showNotification(message) {
+                        // Buat elemen div untuk popup notifikasi
+                        var notificationDiv = document.createElement('div');
+                        notificationDiv.className = 'notification-popup';
+                        notificationDiv.innerText = message;
+            
+                        // Tambahkan elemen div ke dalam body
+                        document.body.appendChild(notificationDiv);
+            
+                        // Hapus popup notifikasi setelah beberapa detik (misalnya 3 detik)
+                        setTimeout(function () {
+                            document.body.removeChild(notificationDiv);
+                        }, 3000);
+                    }
+            
+                    // Panggil fungsi showNotification dengan pesan notifikasi dari sesi
+                    showNotification("{{ session('Vehi-success') }}");
+                </script>
+              @endif
+
                 <!-- Striped Rows -->
                 <div class="card">
                   <div class="table-responsive text-nowrap">
