dbb0e8675b8a20cc5263d47415911a8760eba0c0
irwanto1998 <rioirwanto9@gmail.com>
2023-08-01 14:56:41 +0700

    add modal add new service

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -489,6 +489,25 @@
         </div>
     </div>
   </div>
+
+  <!-- Add Confirmation Modal -->
+  <div class="modal fade" id="addSConfirmationModal" tabindex="-1" role="dialog" aria-labelledby="addSConfirmationModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addSConfirmationModalLabel">Konfirmasi</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Ingin menyimpan data ?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary" id="addSButton">Yes</button>
+            </div>
+        </div>
+    </div>
+  </div>
   
   <!-- Edit Confirmation Modal -->
   <div class="modal fade" id="editServiceConfirmationModal" tabindex="-1" role="dialog" aria-labelledby="editServiceConfirmationModalLabel" aria-hidden="true">
